413d27758da86b47d0a1380500e8b5a289cb8c2e
frangarciag <francisco_garcia@trimble.com>
2025-04-09 17:19:25 +0200

    Add files via upload
    Change MIPI

---
+++ Mipichange.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to open the Cheese webcam application
+
+echo "Opening the Cheese webcam application..."
+
+# Command to open Cheese
+cheese &
+
+echo "The Cheese application should now be open."
+
+exit 0
